00eb5be5a0b1bffa1118e0772a81d984ce4d341f
Stephen Balogh <sgbalogh@gmail.com>
2016-04-21 20:30:51 +0000

    makes 'dct_isPartOf and 'dc_publisher' link to searches

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -118,8 +118,8 @@ class CatalogController < ApplicationController
     # helper_method: [Symbol] method that can be used to render the value
     config.add_show_field 'dc_creator_sm', label: 'Author(s)', itemprop: 'author'
     config.add_show_field 'dc_description_s', label: 'Description', itemprop: 'description', helper_method: :render_value_as_truncate_abstract
-    config.add_show_field 'dc_publisher_s', label: 'Publisher', itemprop: 'publisher'
-    config.add_show_field 'dct_isPartOf_sm', label: 'Collection', itemprop: 'isPartOf'
+    config.add_show_field 'dc_publisher_s', label: 'Publisher', itemprop: 'publisher', link_to_search: true
+    config.add_show_field 'dct_isPartOf_sm', label: 'Collection', itemprop: 'isPartOf', link_to_search: true
     config.add_show_field 'dct_spatial_sm', label: 'Place(s)', itemprop: 'spatial', link_to_search: true, separator: '; '
     config.add_show_field 'dc_subject_sm', label: 'Subject(s)', itemprop: 'keywords', link_to_search: true
     config.add_show_field 'dct_temporal_sm', label: 'Year', itemprop: 'temporal'

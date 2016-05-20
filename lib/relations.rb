d0f89ee9dbca8de21cbe9cb63aa70588b286f089
Stephen Balogh <sgbalogh@gmail.com>
2016-05-20 12:11:03 -0400

    fixes typos!

--- lib/relations.rb
+++ lib/relations.rb
@@ -9,22 +9,22 @@ class Relations
       @ancestors = solr.get 'select', :params => {:q => "layer_slug_s:#{@document['dct_source_sm'].join('OR')}", :fl => ['dc_title_s', 'layer_slug_s']}
     end
 
-    @descendents = solr.get 'select', :params => {:q => "dct_source_sm:#{@document['layer_slug_s']}", :fl => ['dc_title_s', 'layer_slug_s']}
+    @descendants = solr.get 'select', :params => {:q => "dct_source_sm:#{@document['layer_slug_s']}", :fl => ['dc_title_s', 'layer_slug_s']}
 
     @count = {}
     @count[:ancestors] = @ancestors['response']['numFound'] unless @ancestors.blank?
-    @count[:descendents] = @descendents['response']['numFound'] unless @descendents['response']['numFound'] == 0
+    @count[:descendants] = @descendants['response']['numFound'] unless @descendants['response']['numFound'] == 0
 
     @search = {}
-    @search[:descendents] = "catalog?f[dct_source_sm][]=#{@document['layer_slug_s']}"
+    @search[:descendants] = "catalog?f[dct_source_sm][]=#{@document['layer_slug_s']}"
   end
 
   def ancestors
     @ancestors['response']['docs'] unless @ancestors.blank?
   end
 
-  def descendents
-    @descendents['response']['docs']
+  def descendants
+    @descendants['response']['docs']
   end
 
   def count

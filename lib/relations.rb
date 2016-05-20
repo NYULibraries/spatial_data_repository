bc19c2facddcbc088defbab5d26e7aac8f55e4f1
Stephen Balogh <sgbalogh@gmail.com>
2016-05-20 12:07:01 -0400

    refactors Relations; adds icons for ancestor/descendant

--- lib/relations.rb
+++ lib/relations.rb
@@ -2,34 +2,37 @@ class Relations
 
   def initialize(document)
     @document = document
-    @solr = RSolr.connect :url => ENV['SOLR_URL']
+    solr = RSolr.connect :url => ENV['SOLR_URL']
+
+    @ancestors = []
+    unless @document['dct_source_sm'].blank?
+      @ancestors = solr.get 'select', :params => {:q => "layer_slug_s:#{@document['dct_source_sm'].join('OR')}", :fl => ['dc_title_s', 'layer_slug_s']}
+    end
+
+    @descendents = solr.get 'select', :params => {:q => "dct_source_sm:#{@document['layer_slug_s']}", :fl => ['dc_title_s', 'layer_slug_s']}
+
     @count = {}
+    @count[:ancestors] = @ancestors['response']['numFound'] unless @ancestors.blank?
+    @count[:descendents] = @descendents['response']['numFound'] unless @descendents['response']['numFound'] == 0
+
     @search = {}
+    @search[:descendents] = "catalog?f[dct_source_sm][]=#{@document['layer_slug_s']}"
   end
 
   def ancestors
-    unless @document['dct_source_sm'].blank?
-      query = @solr.get 'select', :params => {:q => "layer_slug_s:#{@document['dct_source_sm'].join('OR')}", :fl => ['dc_title_s', 'layer_slug_s']}
-      @count[:ancestors] = query['response']['numFound']
-      query['response']['docs']
-    end
+    @ancestors['response']['docs'] unless @ancestors.blank?
   end
 
   def descendents
-    query = @solr.get 'select', :params => {:q => "dct_source_sm:#{@document['layer_slug_s']}", :fl => ['dc_title_s', 'layer_slug_s']}
-    unless query['response']['numFound'] == 0
-      @count[:descendents] = query['response']['numFound']
-      @search[:descendents] = "catalog?f[dct_source_sm][]=#{@document['layer_slug_s']}"
-    end
-    query['response']['docs']
-  end
-
-  def exist?
-    !ancestors.blank? || !descendents.blank?
+    @descendents['response']['docs']
   end
 
   def count
     @count
+  end
+
+  def exist?
+    !@count.blank?
   end
 
   def search

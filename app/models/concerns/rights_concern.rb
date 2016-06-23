dd3ebac5baae0a6414bbf32ce2faff4b2097ed0c
Stephen Balogh <sgbalogh@gmail.com>
2016-06-23 17:09:37 -0400

    slight language change; link color change

--- app/models/concerns/rights_concern.rb
+++ app/models/concerns/rights_concern.rb
@@ -3,7 +3,7 @@ module RightsConcern
 
   def rights_text
     if nyu? && restricted?
-      "This dataset is only available to members of the <b>New York University</b> community, limited to <em>current</em> students, staff, and faculty of the university. Do not copy or redistribute this data. If you have questions about what constitutes <b>fair use</b> of this material, please contact us at <a href='mailto:data.services@nyu.edu'>data.services@nyu.edu</a>."
+      "This dataset is only available to members of the <b>New York University</b> community, limited to <em>current</em> students, staff, and faculty of the university. Do not copy or redistribute this data. If you have questions about what constitutes fair use of this material, please contact us directly at <a href='mailto:data.services@nyu.edu'>data.services@nyu.edu</a>."
     end
   end
 

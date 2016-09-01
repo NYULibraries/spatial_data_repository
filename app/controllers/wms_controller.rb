5d69497c4120ccd4078ab455d1eba74dd34d73e6
Stephen Balogh <sgbalogh@gmail.com>
2016-09-01 14:42:54 -0400

    fixes overridden WmsController

--- app/controllers/wms_controller.rb
+++ app/controllers/wms_controller.rb
@@ -1,15 +1,20 @@
 class WmsController < ApplicationController
-before_action :format_url
+  before_action :format_url
   def handle
-    response = Geoblacklight::WmsLayer.new(params).feature_info
+    response = Geoblacklight::WmsLayer.new(wms_params).feature_info
 
     respond_to do |format|
       format.json { render json: response }
     end
   end
 
+  private
+
+  def wms_params
+    params.permit('URL', 'LAYERS', 'BBOX', 'WIDTH', 'HEIGHT', 'QUERY_LAYERS', 'X', 'Y')
+  end
+
   def format_url
     params['URL'] = Settings.RESTRICTED_URL if params['URL'].include? Settings.PROXY_URL
- end
-
+  end
 end

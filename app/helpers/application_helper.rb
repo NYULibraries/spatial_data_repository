25ee6f1b80dfb1ab28047157f85813f0b6d057ed
Stephen Balogh <sgbalogh@gmail.com>
2017-01-09 17:21:45 -0500

    additional cosmetic changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,93 +2,87 @@ module ApplicationHelper
 
   def construct_curated_maps
     maps = NyuGeoblacklight::CuratedCollections.maps.shuffle[0..2]
-    toReturn = "  <div class=\"curated-maps\">
+    toReturn = "<div class=\"curated-maps\">
       <h4>Featured Maps</h4>
       <div class=\"row\">"
-
     maps.each do |map|
       toReturn += create_map_portion(map)
     end
-
     toReturn += "</div>
-                </div>
-              "
-
+                </div>"
     toReturn.html_safe
   end
 
   def create_map_portion(map)
-    "<a style=\"display:block\" href=\"./catalog/#{map[:slug]}\">
+    toReturn = "<a class=\"curated-card\" style=\"display:block\" href=\"./catalog/#{map[:slug]}\">
           <div class=\"col-md-4\">
-            <div class=\"card-map\">
-              <img class=\"card-img-top\" src=\"./assets/thumb/#{map[:slug]}.jpg\" alt=\"Card image cap\">
-
-              <div class=\"card-block\">
+            <div class=\"card-map\">"
+    toReturn += (image_tag "thumb/#{map[:slug]}.jpg", {:class => "card-img-top", :"data-svg-fallback" => "#{image_path('real250.png')}", :alt => 'NYU Spatial Data Repository'})
+    toReturn += "<div class=\"card-block\">
                 <h4 class=\"card-title\">#{map[:title]}</h4>
               </div>
             </div>
           </div>
         </a>"
+    toReturn
   end
 
   def construct_curated_collections
     collections = NyuGeoblacklight::CuratedCollections.collections.shuffle[0..1]
     toReturn = "<div class=\"curated-collections\">
-
       <h4>Featured Collections</h4>
-
       <div class=\"row\">"
-
     collections.each do |collection|
       toReturn += create_collection_portion(collection)
     end
-
     toReturn += " </div>
-    </div>
-          "
+    </div>"
     toReturn.html_safe
   end
 
   def create_collection_portion(collection)
-    "<div class=\"col-md-6\">
+    toReturn = link_to search_catalog_path({f: collection[:f]}), {class: "curated-card"} do
+      "
+    <div class=\"col-md-6\">
           <div class=\"card-collection\">
             <div class=\"card-block\">
               <h4 class=\"card-title\">#{collection[:title]}</h4>
-
               <p class=\"card-text\">#{collection[:description]}</p>
             </div>
           </div>
-        </div>"
+        </div>
+    ".html_safe
+    end
+    toReturn.html_safe
   end
 
   def construct_recent_collections
     collections = NyuGeoblacklight::CuratedCollections.recent[0..1]
     toReturn = "<div class=\"curated-collections\">
-
       <h4 class=\"collection-section-title\">Recently Updated</h4>
-
       <div class=\"row\">"
-
     collections.each do |collection|
       toReturn += create_recent_portion(collection)
     end
-
     toReturn += " </div>
-    </div>
-          "
+    </div>"
     toReturn.html_safe
   end
 
   def create_recent_portion(collection)
-    "<div class=\"col-md-6\">
+    toReturn = link_to search_catalog_path({f: collection[:f]}), {class: "curated-card"} do
+      "
+    <div class=\"col-md-6\">
           <div class=\"card-collection\">
             <div class=\"card-block\">
               <h4 class=\"card-title\">#{collection[:title]}</h4>
-
               <p class=\"card-text\">#{collection[:description]}</p>
             </div>
           </div>
-        </div>"
+        </div>
+    ".html_safe
+    end
+    toReturn.html_safe
   end
 
 
--- lib/nyugeoblacklight/curated_collections.rb
+++ lib/nyugeoblacklight/curated_collections.rb
@@ -3,17 +3,19 @@ module NyuGeoblacklight
 
     def CuratedCollections.collections
       [
-          {title: "New York City Open Data", description: "A collection of datasets related to NYC infrastructure, real estate, transportation, and political geography", href: "./?f[dc_rights_s][]=Public&f[dct_spatial_sm][]=New York, New York, United States"},
-          {title: "NYU Research Data", description: "Geospatial data submitted for preservation by NYU students, faculty, and staff", href: ""},
-          {title: "U.S. Census Data", description: "Datasets which package data from the U.S. Census, or American Community Survey, along with geographic boundaries", href: ""},
-          {title: "Soviet Topographic Maps of the Arabian Peninsula", description: "Raster scans of Soviet military topographic maps produced in the late 1970s", href: ""}
+          {title: "New York City Open Data", description: "A collection of datasets related to NYC infrastructure, real estate, transportation, and political geography", f: { "dc_rights_s" => ["Public"], "dct_spatial_sm" => ["New York, New York, United States"]}},
+          {title: "NYU Research Data", description: "Geospatial data submitted for preservation by NYU students, faculty, and staff", f: {"dct_isPartOf_sm" => ["NYU Research Data"]}},
+          #{title: "U.S. Census Data", description: "Datasets which package data from the U.S. Census, or American Community Survey, along with geographic boundaries", f: {"dct_isPartOf_sm" => ["United States Census Data"]}},
+          #{title: "Soviet Topographic Maps of the Arabian Peninsula", description: "Raster scans of Soviet military topographic maps produced in the late 1970s", f: { "dc_publisher_s" => ["Omni Resources (Firm)"]}}
+          {title: "Latin American Census Geodatabases", description: "ESRI Geodatabases of census data from Latin American countries", f: {"dct_isPartOf_sm" => ["Latin American Census Data"]}},
+          {title: "Vector Map 1 (VMap1) Layers", description: "Highly detailed vector layers depecting terrain, topology, transportation, and civil infrastructure", f: {"dct_isPartOf_sm" => ["VMap 1"]}},
       ]
     end
 
     def CuratedCollections.recent
       [
-          {title: "Mexico Encuesta Intercensal 2015", description: "Layers from the 2015 Mexican Intercensal Survey", href: "./?f[dct_isPartOf_sm][]=Encuesta Intercensal 2015"},
-          {title: "Soviet Topographic Maps of the Arabian Peninsula", description: "Raster scans of Soviet military topographic maps produced in the late 1970s", href: ""}
+          {title: "Mexico Encuesta Intercensal 2015", description: "Layers from the 2015 Mexican Intercensal Survey", f: {"dct_isPartOf_sm" => ["Encuesta Intercensal 2015"]}},
+          {title: "Soviet Topographic Maps of the Arabian Peninsula", description: "Raster scans of Soviet military topographic maps produced in the late 1970s", f: { "dc_publisher_s" => ["Omni Resources (Firm)"]}}
       ]
     end
 

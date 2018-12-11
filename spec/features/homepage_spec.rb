3e514a660cf7627efd0bfa50ff6bf1346587c557
Eric Larson <ewlarson@gmail.com>
2018-12-11 15:59:41 -0600

    RSpec: stub homepage features

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Homepage' do
+  describe 'DOM' do
+    it 'has Featured Collections' do
+      visit root_path
+      expect(page).to have_content 'Featured Collections'
+      expect(page).to have_selector '.curated-collections'
+    end
+
+    it 'has Featured Maps' do
+      visit root_path
+      expect(page).to have_content 'Featured Maps'
+      expect(page).to have_selector '.curated-maps'
+    end
+  end
+end

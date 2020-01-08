34f6831516a56a39b8520cc5cdca92fa11e9c1aa
mnyrop <marii@nyu.edu>
2020-01-08 16:20:35 -0500

    rm extras

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -8,9 +8,10 @@ module Features
     # Use this in feature tests
     def sign_in(who = :user)
       user = create(:user)
-      extra = OpenStruct.new(identities: ["foo"])
+      # extra = OpenStruct.new(identities: ["foo"])
 
-      OmniAuth.config.add_mock(:nyulibraries, uid: user, extra: extra)
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.add_mock(:nyulibraries, uid: user) #, extra: extra)
       visit user_nyulibraries_omniauth_authorize_path
     end
   end

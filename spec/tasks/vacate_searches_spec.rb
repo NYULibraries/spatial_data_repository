9432f64f287158af108f52da181a0997813a0c30
Michael Cain <michael.m.cain.ii@gmail.com>
2023-06-22 07:41:07 -0500

    update Gemfile and write/test task to vacate old Searches

--- spec/tasks/vacate_searches_spec.rb
+++ spec/tasks/vacate_searches_spec.rb
@@ -1,9 +1,7 @@
 require 'timecop'
 
-RSpec.describe "Vacate Searches", :type => :task do
-
-  context "vacate_searches:vacate" do
-
+RSpec.describe 'Vacate Searches', type: :task do
+  context 'vacate_searches:vacate' do
     before do
       # Freeze time as task is time-sensitive
       Timecop.freeze Time.now
@@ -11,27 +9,24 @@ RSpec.describe "Vacate Searches", :type => :task do
 
     after { Timecop.return }
 
-    it "deletes searches older than 1 week" do
+    it 'deletes searches older than 1 week' do
       create(:search, created_at: 2.week.ago)
       expect { invoke_task }.to change { Search.count }.by(-1)
     end
 
-    it "does not delete newer searches" do
+    it 'does not delete newer searches' do
       create(:search, created_at: 2.days.ago)
-      expect { invoke_task }.not_to change { Search.count }
+      expect { invoke_task }.not_to(change { Search.count })
     end
 
     private
 
     def invoke_task
-      task = Rake::Task["vacate_searches:vacate"]
+      task = Rake::Task['vacate_searches:vacate']
       # Ensure task is re-enabled, as rake tasks by default are disabled
       # after running once within a process http://pivotallabs.com/how-i-test-rake-tasks/
       task.reenable
       task.invoke
     end
-
   end
-
-
 end

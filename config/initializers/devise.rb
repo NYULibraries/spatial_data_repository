14d19ac0295ae6083cc4a76798d860c78e184641
Michael Cain <mmc469@nyu.edi>
2023-08-17 14:03:18 -0500

    add missing auth code config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,6 +18,7 @@ Devise.setup do |config|
                   authorize_params: { scope: "openid" },
                   client_options: { site: (Settings.LOGIN_URL || "https://qa.auth.it.nyu.edu"),
                                     authorize_url: "/oauth2/authorize",
-                                    token_url: "/oauth2/token", }
+                                    token_url: "/oauth2/token",
+                                    response_type: 'authorization_code'}
 
 end

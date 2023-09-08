916f21186c55fc3863b13b1ee5bab75a65d9859b
Michael Cain <mmc469@nyu.edi>
2023-09-08 09:07:08 -0500

    rubocop again...

--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -2,8 +2,9 @@ describe 'Accessibility', type: :feature do
   before do
     sign_in
   end
+
   it 'has no accessibility errors' do
     visit bookmarks_path
-    expect(page).to be_axe_clean.within ('body')
+    expect(page).to be_axe_clean.within('body')
   end
 end

dfb31640d1e479fc5a1a05a30206f6bb6ac64635
Michael Cain <mmc469@nyu.edi>
2023-11-21 07:47:02 -0600

    add production deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "52.4.204.0", user: "ubuntu", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -41,12 +41,17 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
+set :ssh_options, {
+  auth_methods: %w(publickey),
+  forward_agent: true,
+  keys: %W(~/.ssh/key-1-jun24-2015.pem),
+  user: 'ubuntu'
+}
+
+set :branch, "main"
+
+set :rails_env, 'production'
+
 # The server-based syntax can be used to override options:
 # ------------------------------------
 # server "example.com",

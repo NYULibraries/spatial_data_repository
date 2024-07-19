73ece1969e6d496377cea84c2f9029143c698504
Brian Kelly <polymonic@gmail.com>
2024-07-19 14:19:04 -0500

    Change the from address we're using

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_options = {from: 'no-reply@geo.nyu.edu'}
+  config.action_mailer.default_options = {from: 'lib-no-reply@nyu.edu'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

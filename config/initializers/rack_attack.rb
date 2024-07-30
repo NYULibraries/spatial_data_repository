b8db6f8cc65651828bedd7056eeab028ea6d51ce
Brian Kelly <polymonic@gmail.com>
2024-07-30 12:04:24 -0500

    Block unsavory crawlers/bots that are generating a lot of traffic

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,4 +1,9 @@
 class Rack::Attack
+  # Crawlers, Bots & Troublemakers
+  blocklist_ip("47.76.209.138") # Alibaba Cloud HK
+  blocklist_ip("47.76.99.127")  # Alibaba Cloud HK
+  blocklist_ip("34.211.15.100") # Overly Curious EC2 Instance
+
   # Throttle all requests by IP (60rpm)
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"

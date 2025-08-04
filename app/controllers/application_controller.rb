6cb416bb31aa3737458c6d439ba7e82b2d92f231
Brian Kelly <polymonic@gmail.com>
2025-08-04 15:05:22 -0500

    Bump gems and fix rubocop issues (#403)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
+
   layout :determine_layout if respond_to? :layout
 
   before_action :allow_geoblacklight_params

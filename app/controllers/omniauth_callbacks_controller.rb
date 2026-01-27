648561888eb18ba6d8e1b6078f4508fed2bcb7e3
Brian Kelly <brian@spilth.org>
2026-01-27 12:05:52 -0600

    Attempted Capistrano and Entra ID fixes

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token, only: :entra_id
+
+  def entra_id
+    Rails.logger.info("OmniauthCallbacksController#entra_id: #{omniauth.inspect}")
+    set_user
+    if @user.persisted?
+      log_in
+    else
+      redirect_to_login
+    end
+  end
+
   def failure
     redirect_to root_path
   end
